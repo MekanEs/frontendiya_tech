2f51b474ed3ecc686205f506f9ec19e747f3bd80
MekanEs <Mekan.es1997@gmail.com>
2023-04-01 23:21:10 +0300

    feat: made themeChanging

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { FC } from 'react';
+import React, { FC, useState } from 'react';
 
-import { Test } from './components';
+import { Button, MantineProvider } from '@mantine/core';
 
 export const App: FC = () => {
+  const [color, setColor] = useState('dark');
+  const change: () => void = () => {
+    if (color === 'dark') {
+      setColor('light');
+    } else {
+      setColor('dark');
+    }
+    console.log(color);
+  };
+
   return (
-    <div>
-      <Test />
-    </div>
+    // @ts-ignore
+    <MantineProvider theme={{ colorScheme: color }} withGlobalStyles withNormalizeCSS>
+      <div>
+        <Button onClick={change} variant="filled">
+          Change theme
+        </Button>
+      </div>
+    </MantineProvider>
   );
 };
--- src/components/Test/Test.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-
-export const Test: FC = () => {
-  return <div>Hello world Mekan!</div>;
-};
